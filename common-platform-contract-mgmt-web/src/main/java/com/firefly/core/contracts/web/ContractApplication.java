1ecb7be8b5b600184fc3189358bede256cb9283e
Andrés Contreras Guillén <me@ancongui.com>
2025-09-08 13:05:33 +0200

    Replace catalis references with firefly - core.catalis.vc -> core.getfirefly.io and catalis -> firefly

--- common-platform-contract-mgmt-web/src/main/java/com/firefly/core/contracts/web/ContractApplication.java
+++ common-platform-contract-mgmt-web/src/main/java/com/firefly/core/contracts/web/ContractApplication.java
@@ -52,7 +52,7 @@ import org.springframework.web.reactive.config.EnableWebFlux;
         ),
         servers = {
                 @Server(
-                        url = "http://core.catalis.vc/contracts",
+                        url = "http://core.getfirefly.io/contracts",
                         description = "Development Environment"
                 ),
                 @Server(
